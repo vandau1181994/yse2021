d3ed4ca5cf9969d834cc300a4b3da5a929860020
kimminwoo1 <200039jt@yse-c.net>
2021-10-13 12:03:11 +0900

    abc

--- nyuka.php
+++ nyuka.php
@@ -49,11 +49,15 @@ try
 
 
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-
+if(empty($_POST["books"])){
 	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
-
+	$_SESSION["success"] = "入荷する商品が選択されていません";
 	//⑩在庫一覧画面へ遷移する。
-	
+	header("Location: zaiko_ichiran.php");
+}else
+{
+	unset($_SESSION["success"]);
+}
 
 //var_dump($_POST);
 function getId($id,$con){
