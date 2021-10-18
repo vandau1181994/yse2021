b0d5c23a88aa4a63bbd2549848e92b5be2da6cc4
vandau1181994 <200263yw@yse-c.net>
2021-10-18 10:51:33 +0900

    logout.php

--- logout.php
+++ logout.php
@@ -3,9 +3,12 @@
 【機能】
 	セッション情報を削除しログイン画面に遷移する。
 */
+
 //①セッションを開始する。
-
+session_start();
 //②セッションを削除する。
-
+unset($_SESSION["id"]);
+unset($_SESSION["name"]);
 //③ログイン画面へ遷移する。
+header("Location:login.php")
 ?>
