11ba3846d2c9d0556a24dd341dbe24764584c550
kimminwoo1 <200039jt@yse-c.net>
2021-10-20 12:01:38 +0900

    nyuka

--- nyuka.php
+++ nyuka.php
@@ -23,13 +23,13 @@ if (session_status() == PHP_SESSION_NONE) {
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
 
-
+if ( !isset($_SESSION["user"]) ||$_SESSION["login"]==false){
 	//④SESSIONの「error2」に「ログインしてください」と設定する。
-
+	$_SESSION["error2"] = "ログインしてください";
 	//⑤ログイン画面へ遷移する。
-
-	
-
+	header("Location: login.php");
+	exit;
+}
 
 //⑥データベースへ接続し、接続情報を変数に保存する
 $dbname = "zaiko2021_yse";
@@ -112,9 +112,11 @@ function getId($id,$con){
 			//var_dump($_POST["books"]);
 			
 			
-			// if(/* ⑬の処理を書く */){
-			// 	//⑭SESSIONの「error」の中身を表示する。
-			// }
+			if(isset($_SESSION["error"])){
+				//⑭SESSIONの「error」の中身を表示する。
+				echo '<p>'.$_SESSION["error"].'</p>';
+				$_SESSION["error"]="";
+			}
 			?>
 			</div>
 			<div id="center">
