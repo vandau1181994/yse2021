cde7f4914112d22c880b885c21e6d8be979584c8
vandau1181994 <200263yw@yse-c.net>
2021-11-01 11:48:17 +0900

    nyuka_kakunin

--- nyuka_kakunin.php
+++ nyuka_kakunin.php
@@ -10,39 +10,47 @@
 //①セッションを開始する
 session_start();
 
-function getByid($id,$con){
+function getByid($id, $con)
+{
 	/* 
 	 * ②書籍を取得するSQLを作成する実行する。
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
-	$id=htmlspecialchars($id);
-	$sql="SELECT * FROM books WHERE id={$id}";
-	$statement=$con->query($sql);
+	$id = htmlspecialchars($id);
+	$sql = "SELECT * FROM books WHERE id={$id}";
+	$statement = $con->query($sql);
 	//③実行した結果から1レコード取得し、returnで値を返す。
-	$items=$statement->fetch(PDO::FETCH_ASSOC);
+	$items = $statement->fetch(PDO::FETCH_ASSOC);
 	return $items;
 }
 
-function updateByid($id,$con,$total){
+function updateByid($id, $con, $total)
+{
 	/*
 	 * ④書籍情報の在庫数を更新するSQLを実行する。
 	 * 引数で受け取った$totalの値で在庫数を上書く。
 	 * その際にWHERE句でメソッドの引数に$idに一致する書籍のみ取得する。
 	 */
-    $sql="UPDATE books SET stock={$total} WHERE id={$id}";
-    $con->query($sql);
-
+	$sql = "UPDATE books SET stock={$total} WHERE id={$id}";
+	$con->query($sql);
 }
 
 //⑤SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if ($_SESSION["login"] == false){
+if ($_SESSION["login"] == false) {
 	//⑥SESSIONの「error2」に「ログインしてください」と設定する。
 	$_SESSION["error2"] = "ログインしてください";
 	//⑦ログイン画面へ遷移する。
 	header("Location: login.php");
 }
 ///// dont copy <div class=""></div>
+if (empty($_POST["books"])) {
+	//⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
+	//⑩在庫一覧画面へ遷移する。
+	$_SESSION["success"] = "入荷する商品が選択されていません";
+	header("Location: zaiko_ichiran.php");
+	exit;
+}
 
 //⑧データベースへ接続し、接続情報を変数に保存する
 $dbname = "zaiko2021_yse";
@@ -50,54 +58,52 @@ $host = "localhost";
 $charset = "UTF8";
 $user =  "zaiko2021_yse";
 $password = "2021zaiko";
-$option = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
+$option = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 //⑨データベースで使用する文字コードを「UTF8」にする
 $dsn = "mysql:dbname={$dbname};host={$host};charset={$charset}";
-try
-{
-	$pdo = new PDO($dsn,$user,$password,$option);
+try {
+	$pdo = new PDO($dsn, $user, $password, $option);
 	// echo "SUCCESS";
-}catch(PDOException $e)
-{
+} catch (PDOException $e) {
 	die($e->getMessage());
 }
 //⑩書籍数をカウントするための変数を宣言し、値を0で初期化する
-$count=0;
+$count = 0;
 //⑪POSTの「books」から値を取得し、変数に設定する。
 
-foreach($_POST['books'] as$book_id){
+foreach ($_POST['books'] as $book_id) {
 	/*
 	 * ⑫POSTの「stock」について⑩の変数の値を使用して値を取り出す。
 	 * 半角数字以外の文字が設定されていないかを「is_numeric」関数を使用して確認する。
 	 * 半角数字以外の文字が入っていた場合はif文の中に入る。
 	 */
-	$stock=$_POST['stock'][$count];
-	
+	$stock = $_POST['stock'][$count];
+
 	if (!is_numeric($stock)) {
 		//⑬SESSIONの「error」に「数値以外が入力されています」と設定する。
-		$_SESSION['error']="数値以外が入力されています";
+		$_SESSION['error'] = "数値以外が入力されています";
 		//⑭「include」を使用して「nyuka.php」を呼び出す。
 		include 'nyuka.php';
 		//⑮「exit」関数で処理を終了する。
 		exit;
 	}
-//// dont copy <div class=""></div>
+	//// dont copy <div class=""></div>
 	//⑯「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に⑪の処理で取得した値と⑧のDBの接続情報を渡す。
-    $book=getByid($book_id,$pdo);
+	$book = getByid($book_id, $pdo);
 	//⑰ ⑯で取得した書籍の情報の「stock」と、⑩の変数を元にPOSTの「stock」から値を取り出し、足した値を変数に保存する。
-    $total_stock=$book['stock']+$stock;
+	$total_stock = $book['stock'] + $stock;
 	//⑱ ⑰の値が100を超えているか判定する。超えていた場合はif文の中に入る。
-	if($total_stock>100){
+	if ($total_stock > 100) {
 		//⑲SESSIONの「error」に「最大在庫数を超える数は入力できません」と設定する。
-		$_SESSION['error']="最大在庫数を超える数は入力できません";
+		$_SESSION['error'] = "最大在庫数を超える数は入力できません";
 		//⑳「include」を使用して「nyuka.php」を呼び出す。
 		include 'nyuka.php';
 		//㉑「exit」関数で処理を終了する。
 		exit;
 	}
-	
+
 	//㉒ ⑩で宣言した変数をインクリメントで値を1増やす。
-	
+
 	$count++;
 }
 
@@ -107,25 +113,25 @@ foreach($_POST['books'] as$book_id){
  * 
  * 値が入っている場合は中身に「ok」が設定されていることを確認する。
  */
-if(isset($_POST['add'])){
+if (isset($_POST['add'])) {
 	//㉔書籍数をカウントするための変数を宣言し、値を0で初期化する。
-    $count=0;
+	$count = 0;
 
 	//㉕POSTの「books」から値を取得し、変数に設定する。
-	foreach ($_POST['books'] as $book_id){
+	foreach ($_POST['books'] as $book_id) {
 		//㉖「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に㉕の処理で取得した値と⑧のDBの接続情報を渡す。
-		$book=getByid($book_id,$pdo);
+		$book = getByid($book_id, $pdo);
 		//㉗ ㉖で取得した書籍の情報の「stock」と、㉔の変数を元にPOSTの「stock」から値を取り出し、足した値を変数に保存する。
-		
-		$total_stock=$book['stock']+$_POST['stock'][$count];
+
+		$total_stock = $book['stock'] + $_POST['stock'][$count];
 		//㉘「updateByid」関数を呼び出す。その際に引数に㉕の処理で取得した値と⑧のDBの接続情報と㉗で計算した値を渡す。
-		updateByid($book_id,$pdo,$total_stock);
+		updateByid($book_id, $pdo, $total_stock);
 		//㉙ ㉔で宣言した変数をインクリメントで値を1増やす。
 		$count++;
 	}
 
 	//㉚SESSIONの「success」に「入荷が完了しました」と設定する。
-	$_SESSION['success']="入荷が完了しました";
+	$_SESSION['success'] = "入荷が完了しました";
 	//㉛「header」関数を使用して在庫一覧画面へ遷移する。
 	header('Location:zaiko_ichiran.php');
 	exit;
@@ -133,11 +139,13 @@ if(isset($_POST['add'])){
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
 	<meta charset="UTF-8">
 	<title>入荷確認</title>
 	<link rel="stylesheet" href="css/ichiran.css" type="text/css" />
 </head>
+
 <body>
 	<div id="header">
 		<h1>入荷確認</h1>
@@ -155,22 +163,22 @@ if(isset($_POST['add'])){
 					</thead>
 					<tbody>
 						<?php
-						//㉜書籍数をカウントするための変数を宣言し、値を0で初期化する
-                          $count=0;
+						//㉜書籍数をカウントするための変数を宣言し、値を0で初期化する。
+						$count = 0;
 						//㉝POSTの「books」から値を取得し、変数に設定する。
-						foreach($_POST['books']as $book_id){
+						foreach ($_POST['books'] as $book_id) {
 							//㉞「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に㉜の処理で取得した値と⑧のDBの接続情報を渡す。
-							$book = getByid($book_id,$pdo);
+							$book = getByid($book_id, $pdo);
 						?>
-						<tr>
-							<td><?php echo	$book['title'];?></td>
-							<td><?php echo	$book['stock'];?></td>
-							<td><?php echo	$_POST['stock'][$count]?></td>
-						</tr>
-						<input type="hidden" name="books[]" value="<?php echo $book['id']?>">
-						<input type="hidden" name="stock[]" value='<?php echo $_POST['stock'][$count] ;?>'>
+							<tr>
+								<td><?php echo	$book['title']; ?></td>
+								<td><?php echo	$book['stock']; ?></td>
+								<td><?php echo	$_POST['stock'][$count] ?></td>
+							</tr>
+							<input type="hidden" name="books[]" value="<?php echo $book['id'] ?>">
+							<input type="hidden" name="stock[]" value='<?php echo $_POST['stock'][$count]; ?>'>
 						<?php
-							//㊴ ㉜で宣言した変数をインクリメントで値を1増やす..
+							//㊴ ㉜で宣言した変数をインクリメントで値を1増やす。
 							$count++;
 						}
 						?>
@@ -191,4 +199,5 @@ if(isset($_POST['add'])){
 		<footer>株式会社アクロイト</footer>
 	</div>
 </body>
+
 </html>
