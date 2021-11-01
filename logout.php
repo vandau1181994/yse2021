bb1202b216db13102eb504147f2a00a4f57cacd5
vandau1181994 <200263yw@yse-c.net>
2021-11-01 11:51:33 +0900

    logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,20 @@
 <?php
 /* 
 【機能】
-	セッション情報を削除しログイン画面に遷移する
+	セッション情報を削除しログイン画面に遷移する。
 */
-
 //①セッションを開始する。
-session_start();
+
 //②セッションを削除する。
-$_SESSION = array();
-session_destroy();
+
 //③ログイン画面へ遷移する。
-header("Location:login.php");
+session_start();
+
+$_SESSION = array();
+
+session_destroy();
+
+header("location: login.php");
+
+exit;
 ?>
